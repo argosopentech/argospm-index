c6ec9eacd96dfcf0b5b6824def804a4314c52799
P.J. Finlay <pjappdevelopment@gmail.com>
2024-12-18 09:06:18 -0500

    Port Opus-MT models for Argos
    en, cs, da, de, fi, hu, id, sk, uk, es, ko
    
    https://community.libretranslate.com/t/opus-mt-language-models-port-thread/757/12

--- alphabetize.py
+++ alphabetize.py
@@ -2,7 +2,38 @@
 
 import json
 
-with open('index.json') as index_file:
+
+def check_valid(index):
+    """Check for duplicates. This function will print and exit(1) if invalid"""
+    seen_pairs = set()
+    valid = True
+
+    for entry in index:
+        from_code = entry["from_code"]
+        to_code = entry["to_code"]
+        pair = (from_code, to_code)
+        reverse_pair = (to_code, from_code)
+
+        # Check for duplicates
+        if pair in seen_pairs:
+            print(f"Duplicate found! {entry}")
+            valid = False
+        else:
+            seen_pairs.add(pair)
+
+    # Ensure every pair has its reverse
+    for pair in seen_pairs:
+        if (pair[1], pair[0]) not in seen_pairs:
+            print("Missing reverse! {missing_reverse}")
+            valid = False
+
+    if not valid:
+        print("Invalid index.json")
+        exit(1)
+
+
+with open("index.json") as index_file:
     index = json.load(index_file)
-    index.sort(key=lambda x: x['from_name'] + x['to_name'])
+    check_valid(index)
+    index.sort(key=lambda x: x["from_name"] + x["to_name"])
     print(json.dumps(index, indent=4))
